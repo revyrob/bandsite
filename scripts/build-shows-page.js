bd743e2e28a9bfb88260cd78a8d80cd4192ed209
revyrob <kayle.robson@gmail.com>
2022-08-05 17:32:53 -0700

    added form but needs to be changed to an array

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,101 @@
+/**
+* Adds a single item to the DOM
+* 
+* <div class="shows__card">
+<h4 class="shows__card--title">DATE</h4>
+<div class="shows__card--info">Mon Sept 06 2021 
+</div>
+<h4 class="shows__card--title">VENUE</h4>
+<div class="shows__card--info">Ronald Lane
+</div>
+<h4 class="shows__card--title">LOCATION</h4>
+<div class="shows__card--info">San Francisco, CA
+</div>
+<button class="shows__card--buy">BUY TICKETS</button>
+</div>
+*/
+function addShowToDocument(show) {
+    console.log(show)
+    const list = document.querySelector(".showsList");
+    const card = document.createElement("div");
+    card.classList.add("shows__card");
+    
+    //add DATE title
+    const itemTitleDate = document.createElement("h4");
+    itemTitleDate.classList.add("shows__card--title");
+    itemTitleDate.innerText = "DATE";
+    console.log(itemTitleDate);
+
+    //add date from list
+    const itemDate = document.createElement("div");
+    itemDate.classList.add("shows__card--info");
+    itemDate.innerText = show.date;
+
+    //add VENUE title
+    const itemTitleVenue = document.createElement("h4");
+    itemTitleVenue.classList.add("shows__card--title");
+    itemTitleVenue.innerText = "VENUE";
+    console.log(itemTitleVenue);
+
+    //add venue from list
+    const itemVenue = document.createElement("div");
+    itemVenue.classList.add("shows__card--info");
+    itemVenue.innerText = show.venue;
+    
+    //add LOCATION title
+    const itemTitleLocation = document.createElement("h4");
+    itemTitleLocation.classList.add("shows__card--title");
+    itemTitleLocation.innerText = "LOCATION";
+    console.log(itemTitleLocation);
+
+    //add location from list
+    const itemLocation = document.createElement("div");
+    itemLocation.classList.add("shows__card--info");
+    itemLocation.innerText = show.location;
+
+    //add button
+    const itemButton = document.createElement("button");
+    itemButton.classList.add("shows__card--buy");
+    itemButton.innerText = "BUY TICKETS";
+
+    list.appendChild(card);
+    card.appendChild(itemTitleDate);
+    card.appendChild(itemDate);
+    card.appendChild(itemTitleVenue);
+    card.appendChild(itemVenue);
+    card.appendChild(itemTitleLocation);
+    card.appendChild(itemLocation);
+    card.appendChild(itemButton);
+}
+/* 
+*Generate the show list
+*/
+
+function generateShows(show) {
+    const showItem = document.querySelector(".showsList");
+    showItem.innerText = '';
+    show.forEach(function(shows){
+        addShowToDocument(shows);
+        console.log(shows);
+    });
+}
+
+
+//the array for the dates, venues, and location
+const showItems = [
+    {date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Franciso, CA"},
+    {date: "Tues Sept 21 2021", venue: "Pier 3 East", location: "San Franciso, CA"},
+    {date: "Fri Oct 15 2021", venue: "View Lounge", location: "San Franciso, CA"},
+    {date: "Sat Nov 06 2021", venue: "Hyatt Agency", location: "San Franciso, CA"},
+    {date: "Fri Nov 26 2021", venue: "Moscow Center", location: "San Franciso, CA"},
+    {date: "Wed Dec 15 2021", venue: "Press Club", location: "San Franciso, CA"},
+];
+generateShows(showItems);
+
+// this is for my button to buy ticket
+    // const buyTicket = document.getElementsByClassName('.shows__card--buy');
+
+    // buyTicket.addEventListener("click", (event) => {
+    //     console.log('User wants to buy a ticket.');
+    // });
+
--- scripts/index.page.js
+++ scripts/index.page.js
@@ -1,39 +1,32 @@
-// let array = [1, 2, 3, 4];
-// if(array.length === 0) {
-//     console.log('is an empty array');
-// }
-
-// let sum = 0;
-// for(let i = 0; i < array.length; i++) {
-// 	sum += array[i];
-// }
-// return sum;
-
 /*
 * Comment form
 */
+//Need this for Event Listener
 const commentForm = document.querySelector('.form');
-
 //create a const for the journal entries to be added
 const commentEntries = document.querySelector('.comment__added');
 /*
 * Journal Form Submit Event and Handler
-I want to create a similar comment entry like this
-<!-- past comments  from Connor-->
-<div class="comment__previous">
-<div class="comment__user-image">
-<div class="comment__unknown-user"></div>
+*
+******Use as outline for creating form data*******
+
+<div class="comment__user-image"> - -goes on comment entries
+<div class="comment__unknown-user"></div> - goes in user image
 </div>
-<div class="comment__area">
-<div class="comment__user">
-<h4 class="comment__name">Connor Walton</h4>
-<p class="comment__timeStamp">02/17/2021</p>
+<div class="comment__area"> -- goes on comment entries
+<div class="comment__user"> -- goes in comment area
+<h4 class="comment__name">Connor Walton</h4>-- goes in comment area
+<p class="comment__timeStamp">02/17/2021</p>-- goes in comment area
 </div>
-<p class="comment__comment">This is art. This is inexplicable magic 
+<p class="comment__comment">This is art. This is inexplicable magic -- goes on comment entries
 expressed in the purest way, everything 
 that makes up this majestic work 
 deserves reverence. Let us appreciate 
 this for what it is and what it contains.</p>
+</div>
+</div>
+
+
 </div>
 </div>
 */
@@ -41,7 +34,6 @@ commentForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const name = event.target.name.value;
     const comment = event.target.comment.value;
-    
     
     //exterior comment wrapper
     const commentWrapper = document.createElement('div');
@@ -68,19 +60,25 @@ commentForm.addEventListener('submit', (event) => {
     nameElem.classList.add('comment__name');
     nameElem.innerText = name;
     
+    //add time stamp
+    const timeStamp = document.createElement('p');
+    timeStamp.classList.add('comment__timeStamp');
+    timeStamp.innerText = new Date().toLocaleDateString();
+    
     //comment added to div
     const commentElem = document.createElement('p');
     commentElem.classList.add('comment__comment');
     commentElem.innerText = comment;
     
-    commentEntries.appendChild(commentWrapper);
+    commentEntries.prepend(commentWrapper); // adds to the top
     commentWrapper.appendChild(userImgSection);
     userImgSection.appendChild(userImg);
     commentWrapper.appendChild(commentDiv);
     commentDiv.appendChild(commentUserDiv);
     commentUserDiv.appendChild(nameElem);
+    commentUserDiv.appendChild(timeStamp);
     commentDiv.appendChild(commentElem);
-    
+      
     //I need to clear for another entry
     //I need to figure out how to get the new comments to got to the top
 });
