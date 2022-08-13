05241d91d713537a23a14c140f44849a35fb9cbc
revyrob <kayle.robson@gmail.com>
2022-08-12 22:55:24 -0700

    changed the styling to the same as main

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -10,12 +10,12 @@ function fillShowSection(info) {
     const parentElement = document.querySelector("main");
     const showsSection = document.createElement("section");
     showsSection.classList.add("shows");
-    
+
     //add shows header
     const showsHeader = document.createElement("h2");
     showsHeader.classList.add("shows__title");
     showsHeader.innerText = "Shows";
-    
+
     //shows showsList
     const showListDiv  = document.createElement("div");
     showListDiv.classList.add("showsList");
@@ -29,7 +29,7 @@ function fillShowSection(info) {
     topTitleWrapper.classList.add("shows__topTitle");
     
     // for loop for adding info for labels
-    function loopTitles(array){
+   function loopTitles(array){
         let newHeader = ' ';
         for(let i =0; i < array.length; i++) {
             const topTitle  = document.createElement("div");
@@ -45,7 +45,7 @@ function fillShowSection(info) {
             
         } return(newHeader);
     } 
-   
+    // console.log(loopTitles(info));
     loopTitles(info);
     
     parentElement.appendChild(showsSection);
@@ -66,100 +66,104 @@ fillShowSection(tableLabels);
 /*
 * Adds a single item to the DOM
 * For each of the shows in the array
-* Add the API to hear since the information will be going in here
 */
-const showsURL = "https://project-1-api.herokuapp.com/showdates?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4";
-const list = document.querySelector(".showsList");
+function addShowToDocument(show) {
+    const list = document.querySelector(".showsList");
+    const card = document.createElement("div");
+    card.classList.add("shows__card");
+    
+    //add DATE title
+    const itemTitleDate = document.createElement("h4");
+    itemTitleDate.classList.add("shows__card--title");
+    itemTitleDate.innerText = "DATE";
+    
+    //add date from list
+    const itemDate = document.createElement("div");
+    itemDate.classList.add("shows__card--info-dark");
+    itemDate.innerText = show.date;
+    
+    //add VENUE title
+    const itemTitleVenue = document.createElement("h4");
+    itemTitleVenue.classList.add("shows__card--title");
+    itemTitleVenue.innerText = "VENUE";
+    
+    //add venue from list
+    const itemVenue = document.createElement("div");
+    itemVenue.classList.add("shows__card--info");
+    itemVenue.innerText = show.venue;
+    
+    //add LOCATION title
+    const itemTitleLocation = document.createElement("h4");
+    itemTitleLocation.classList.add("shows__card--title");
+    itemTitleLocation.innerText = "LOCATION";
+    
+    //add location from list
+    const itemLocation = document.createElement("div");
+    itemLocation.classList.add("shows__card--info");
+    itemLocation.innerText = show.location;
+    
+    //add button
+    const itemButton = document.createElement("button");
+    itemButton.classList.add("shows__card--buy");
+    itemButton.innerText = "BUY TICKETS";
+    
+    list.appendChild(card);
+    card.appendChild(itemTitleDate);
+    card.appendChild(itemDate);
+    card.appendChild(itemTitleVenue);
+    card.appendChild(itemVenue);
+    card.appendChild(itemTitleLocation);
+    card.appendChild(itemLocation);
+    card.appendChild(itemButton);
+    
+    /* 
+    *event listener for clicking on a show div
+    * there is still an issue with selected will select either the entire card or the titles??
+    */
+    card.addEventListener('click', (event) => {
+        //if an element has a .style-selected class, remove it
+        //if child element is selected add styling
+        //if another card is selected change styling
+        const selectedElement = document.querySelector('.shows__card--selected');
+        // console.log(event.currentTarget);
+        if (selectedElement !== null) {
+            selectedElement.classList.remove('shows__card--selected');
+        }
+        //just the card can be selected
+        card.classList.add('shows__card--selected')
+    });
+    list.appendChild(card);
+    
+}
+/* 
+*Generate the show list
+Calls the addShowToDocument to loop throug the array
+*/
+function generateShows(showList) {
+    const showItem = document.querySelector(".showsList");
+    showItem.innerText = '';
+    showList.forEach(function(show){
+        addShowToDocument(show);
+    });
+}
 
-axios.get(showsURL).then(response => {
-    
-    //call an array of the data
-    const myShowsArray = response.data;
-    
-    //go through a forEach to put the data where it belongs
-    myShowsArray.forEach(myArray => {
-        //get all the divs and titles to start
-        //make each card
-        
-        const card = document.createElement("div");
-        card.classList.add("shows__card");
-        
-        //add DATE title
-        const itemTitleDate = document.createElement("h4");
-        itemTitleDate.classList.add("shows__card--title");
-        itemTitleDate.innerText = "DATE";
-        
-        //add date from list
-        const itemDate = document.createElement("div");
-        itemDate.classList.add("shows__card--info-dark");
-        itemDate.innerText = new Date(myArray.date).toDateString();
-        
-        //add VENUE title
-        const itemTitleVenue = document.createElement("h4");
-        itemTitleVenue.classList.add("shows__card--title");
-        itemTitleVenue.innerText = "VENUE";
-        
-        //add venue from list
-        const itemVenue = document.createElement("div");
-        itemVenue.classList.add("shows__card--info");
-        itemVenue.innerText = myArray.place;
-        
-        //add LOCATION title
-        const itemTitleLocation = document.createElement("h4");
-        itemTitleLocation.classList.add("shows__card--title");
-        itemTitleLocation.innerText = "LOCATION";
-        
-        //add location from list
-        const itemLocation = document.createElement("div");
-        itemLocation.classList.add("shows__card--info");
-        itemLocation.innerText = myArray.location;
-        
-        //add button
-        const itemButton = document.createElement("button");
-        itemButton.classList.add("shows__card--buy");
-        itemButton.innerText = "BUY TICKETS";
-        
-        //list of appended items
-        //this needs to be at the bottom, it will not work properly
-        //with the event listener if they are added under each section
-        //which they apply
-        list.appendChild(card);
-        card.appendChild(itemTitleDate);
-        card.appendChild(itemDate);
-        card.appendChild(itemTitleVenue);
-        card.appendChild(itemVenue);
-        card.appendChild(itemTitleLocation);
-        card.appendChild(itemLocation);
-        card.appendChild(itemButton);
-        
-        /* 
-        *event listener for clicking on a show div
-        *issue with the event listener was solved by changing where the add shows__card--selected
-        *was targetted.  before it was targetted to event.target but if it targets 'card' 
-        *it works
-        */
-        
-        card.addEventListener('click', () => {
-            //if an element has a .style-selected class, remove it
-            //if child element is selected add styling
-            //if another card is selected change styling
-            const selectedElement = document.querySelector('.shows__card--selected');
-            
-            if (selectedElement !== null) {
-                selectedElement.classList.remove('shows__card--selected');
-            }
-            //just the card can be selected
-            card.classList.add('shows__card--selected');
-
-        });
-        list.appendChild(card);
-    })
-    
-    
-    
-}).catch((err) => {
-    console.error(err);
-});
+/* 
+*
+the array for the dates, venues, and location
+*/
+const showItems = [
+    {date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Franciso, CA"},
+    {date: "Tues Sept 21 2021", venue: "Pier 3 East", location: "San Franciso, CA"},
+    {date: "Fri Oct 15 2021", venue: "View Lounge", location: "San Franciso, CA"},
+    {date: "Sat Nov 06 2021", venue: "Hyatt Agency", location: "San Franciso, CA"},
+    {date: "Fri Nov 26 2021", venue: "Moscow Center", location: "San Franciso, CA"},
+    {date: "Wed Dec 15 2021", venue: "Press Club", location: "San Franciso, CA"},
+];
+/*
+/* 
+*Call the generateShows array with the array of objects showItems
+*/
+generateShows(showItems);
 
 
 
